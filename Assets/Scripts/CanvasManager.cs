c76de7d3e17d41287b888fe09eec61b651288187
Sebastian Uitz <uitzsebastian@gmail.com>
2018-07-09 19:55:13 +0200

    scale canvas to phone screen size

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		float matchWidthOrHeight;
+
+		// check the screen ratio
+		if ((Screen.height / Screen.width) <= 1.78) // screen ratio <= 16:9 -> scale with screen height
+		{
+			matchWidthOrHeight = 1.0f;
+		}
+		else // screen ratio > 16:9 -> scale with screen width
+		{
+			matchWidthOrHeight = 0.0f;
+		}
+
+		// set canvas scale type according to settings (depends on screen ratio - smartphone vs tablet)
+		gameObject.GetComponent<CanvasScaler>().matchWidthOrHeight = matchWidthOrHeight;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
